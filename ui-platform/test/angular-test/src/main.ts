9bd19f381c4747f2b940352dffb38181ee0fb8e4
karan-technous <karan@technous.in>
2026-03-25 11:05:58 +0530

    issue in dev watch functionality

--- ui-platform/test/angular-test/src/main.ts
+++ ui-platform/test/angular-test/src/main.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import '@ui-platform/web-components';
-import { defineCustomElements } from '@ui-platform/web-components/loader';
+// import { defineCustomElements } from '@ui-platform/web-components/loader'; // this for production
+import { defineCustomElements } from '../../../packages/web-components/loader'; // for dev purpose
 import { applyTheme, lightTheme } from '@ui-platform/core';
 defineCustomElements();
 applyTheme(lightTheme);
