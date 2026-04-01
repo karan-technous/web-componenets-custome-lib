8325202ce8c7c9d51f111e81f01d363f2aec548c
karan-technous <karan@technous.in>
2026-04-01 10:50:15 +0530

    changes in test file

--- ui-platform/test/angular-test/src/main.ts
+++ ui-platform/test/angular-test/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { applyTheme, darkTheme } from '@ui-platform/web-components';
-applyTheme(darkTheme);
+// import { applyTheme, darkTheme } from '@ui-platform/web-components';
+// applyTheme(darkTheme);
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
--- ui-platform/test/react-test/src/main.tsx
+++ ui-platform/test/react-test/src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { applyTheme, darkTheme } from "@ui-platform/web-components";
+// import { applyTheme, darkTheme } from "@ui-platform/web-components";
 
-applyTheme(darkTheme);
+// applyTheme(darkTheme);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
